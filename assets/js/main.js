a9f5926105b5081e9c24d9fe13a0f62dbebd46a1
Prakshenjay Bhati <prakshenjay@gmail.com>
2024-03-27 19:58:10 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,144 @@
+/*====== MENU =====*/
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 50;
+			  sectionId = current.getAttribute('id')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+		}else{
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+		}                                                    
+	})
+}
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value__accordion-header')
+    
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(open && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
+
+// window.addEventListener('scroll', scrollActive)
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/* ============== SWIPER EXPLORE ============*/
+let swiperExplore = new Swiper(".explore__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        1024: {
+          spaceBetween: 72,
+        },
+      },
+  });
+
+  /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*====== RELLAX =====*/
+var rellax = new Rellax('.parallax');
+
+
+/*====== ANIMATE GSAP ======*/
+/*Navbar*/
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
+gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+
+/*Text*/
+gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
+gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+
+/*Scroll*/
+gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
+
+
+/*====== SCROLL REVEAL SECTION ======*/
+const sr = ScrollReveal({
+    duration: 2500,
+    reset: true
+});
+
+/*Data*/
+sr.reveal('.section__data',{origin: 'left',distance: '70px'}); 
+
+/*Imgs*/
+sr.reveal('.section__img',{origin: 'left',distance: '90px',delay: 200}); 
