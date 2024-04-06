295207f9317ed54f0076d75b93dea9f508d6c8aa
Swastik Pandey <133706046+harsxx@users.noreply.github.com>
2024-04-06 13:37:23 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,24 +22,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-window.addEventListener('scroll', scrollActive)
 
 function scrollActive(){
-  	const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 50;
-			  sectionId = current.getAttribute('id')
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-		}else{
-			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-		}                                                    
-	})
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
 }
+window.addEventListener('scroll', scrollActive)
 
 /*=============== VALUE ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.value__accordion-item')
